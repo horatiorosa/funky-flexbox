1ff91af67a85cc0964bebb5684bd1f7d6425c219
horatio <horatiorosa@aol.com>
2021-02-26 18:52:19 -0500

    autoprefixer work

--- 13 - autoprefixer/gulpfile.js
+++ 13 - autoprefixer/gulpfile.js
@@ -1,14 +1,15 @@
-var gulp = require('gulp');
-var autoprefixer = require('gulp-autoprefixer');
+const gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
 
-
-gulp.task('styles',function() {
-  gulp.src('css/styles.css')
-    .pipe(autoprefixer())
-    .pipe(gulp.dest('build'))
+gulp.task('styles', () => {
+  return gulp.src('css/styles.css')
+  .pipe(autoprefixer())
+  .pipe(gulp.dest('build'))
 });
 
-
-gulp.task('watch',function() {
-  gulp.watch('css/styles.css', ['styles']);
+gulp.task('watch', () => {
+  return gulp.watch(
+    'css/styles.css',
+    gulp.series('styles'));
 });
+
